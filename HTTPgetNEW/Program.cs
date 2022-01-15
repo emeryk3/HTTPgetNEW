517a9e19133664eea39bcadb31f302a8de2e09f8
Kieran Emery <kizwiz@hotmail.co.uk>
2022-01-15 04:58:33 +0000

    Add project files.

---
+++ HTTPgetNEW/Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTPgetNEW
+{
+    class Program
+    {
+        HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            Program program = new Program();
+            await program.GetTodoItems();
+        }
+
+        private async Task GetTodoItems()
+        {
+            string response = await client.GetStringAsync("https://api.chucknorris.io/jokes/random");
+
+            Console.WriteLine(response);
+
+
+            Console.ReadLine();
+        }
+
+    }
+
+    class Todo
+    {
+        public string categories { get; set; }
+        public int id { get; set; }
+        public string value { get; set; }
+    }
+}
